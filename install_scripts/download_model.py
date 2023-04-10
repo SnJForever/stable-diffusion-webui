bc7be5ace1d14c565eb3cd07d472b08b895cfb4b
SnJForever <569252700@qq.com>
2023-04-10 11:49:41 +0800

    add scripts

--- install_scripts/download_model.py
+++ install_scripts/download_model.py
@@ -1,9 +1,16 @@
+from google.colab import drive
+drive.mount('/content/gdrive')
+
 from torch.hub import download_url_to_file
 
-MODEL_PATH='/content/stable-diffusion-webui/models/Stable-diffusion'
+MODEL_PATH='/content/gdrive/stable-diffusion-webui/models/Stable-diffusion'
+LORA_PATH='/content/gdrive/stable-diffusion-webui/models/Lora'
 
 # SD 1.5
 download_url_to_file('https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt', MODEL_PATH+'/v1-5-pruned-emaonly.ckpt', hash_prefix=None, progress=True)
+
+# 模型 https://civitai.com/models/6424/chilloutmix
+download_url_to_file('https://civitai.com/api/download/models/11745', MODEL_PATH+'chilloutmix-Ni-pruned-fp32-fix.safetensors', hash_prefix=None, progress=True)
 
 # https://civitai.com/models/4823/deliberate
 download_url_to_file('https://civitai.com/api/download/models/15236', MODEL_PATH+'/deliberate_v2.safetensors', hash_prefix=None, progress=True)
@@ -16,3 +23,9 @@ download_url_to_file('https://civitai.com/api/download/models/31884', MODEL_PATH
 
 # https://civitai.com/models/27530/xsarchitectural-21futuretechnologycity
 download_url_to_file('https://civitai.com/api/download/models/32964', MODEL_PATH+'/xsarchitectural_.safetensors', hash_prefix=None, progress=True)
+
+# 墨心 https://civitai.com/models/12597/moxin
+download_url_to_file('https://civitai.com/api/download/models/14856', LORA_PATH+'Moxin_10.safetensors', hash_prefix=None, progress=True)
+
+# 舒克走马 https://civitai.com/api/download/models/20143
+download_url_to_file('https://civitai.com/api/download/models/20143', LORA_PATH+'sukezouma11.safetensors', hash_prefix=None, progress=True)
---
+++ install_scripts/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python /content/stable-diffusion-webui/install_scripts/download_model.py
+
+cd /content/stable-diffusion-webui/extensions
+# 下载安装webUI简体中文语言包，tagcomplete补全Tag插件、images-browser
+git clone https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN
+git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+git clone https://github.com/yfszzx/stable-diffusion-webui-images-browser
+
+ln -s /content/gdrive/stable-diffusion-webui/models/Stable-diffusion /content/stable-diffusion-webui/models/Stable-diffusion
+ln -s /content/gdrive/stable-diffusion-webui/models/Lora /content/stable-diffusion-webui/models/Lora
+
