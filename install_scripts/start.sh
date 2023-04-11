0194ebf7de5e1255ecc41f33454667e8de43eb84
SnJForever <569252700@qq.com>
2023-04-11 09:14:21 +0800

    add

--- install_scripts/start.sh
+++ install_scripts/start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-!pip install oss2
+pip install oss2
 pip install fastapi
 
 mkdir -p /content/gdrive/MyDrive/stable-diffusion-webui/models/Stable-diffusion
 mkdir -p /content/gdrive/MyDrive/stable-diffusion-webui/models/Lora
+mkdir -p /content/stable-diffusion-webui/models/Stable-diffusion
+mkdir -p /content/stable-diffusion-webui/models/Lora
 
-python /content/stable-diffusion-webui/install_scripts/download_model.py
+# python /content/stable-diffusion-webui/install_scripts/download_model.py
 
 cd /content/stable-diffusion-webui/extensions
 # 下载安装webUI简体中文语言包，tagcomplete补全Tag插件、images-browser
